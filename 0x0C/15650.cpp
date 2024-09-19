c8e31560481d76c81332a719ab592d926fae0f3a
junssong <xms2007@naver.com>
2024-09-19 13:32:20 +0900

    feat: 15650

---
+++ 0x0C/15650.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int n, m;
+int arr[9];
+
+void recur(int cur, int start) {
+	if (cur == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = start; i <= n; i++) {
+		arr[cur] = i;
+		recur(cur + 1, i + 1);
+	}
+}
+
+
+int main(void) {
+	cin >> n >> m;
+	recur(0, 1);
+}
