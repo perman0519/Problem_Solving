842a3ac44e732210db223b9e950a552889ac24cd
junssong <xms2007@naver.com>
2024-11-20 14:51:10 +0900

    feat: 1003

--- 0x10_dp/1003.cpp
+++ 0x10_dp/1003.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int main() {
 	int t;
 	cin >> t;
+	vector<pair<int, int> > v(41, {0, 0});
+	v[0].first = 1;
+	v[1].second = 1;
+	for (int i = 2; i <= 40; i++) {
+		v[i].first = v[i - 1].first + v[i - 2].first;
+		v[i].second = v[i - 1].second + v[i - 2].second;
+	}
 	while (t--) {
-		vector<pair<int, int> > v(41, {0, 0});
-		v[0].first = 1;
-		v[1].second = 1;
 		int n;
 		cin >> n;
-		for (int i = 2; i <= n; i++) {
-			v[i].first = v[i - 1].first + v[i - 2].first;
-			v[i].second = v[i - 1].second + v[i - 2].second;
-		}
 		cout << v[n].first << ' ' << v[n].second << '\n';
 	}
 	return 0;
