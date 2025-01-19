15d637a97fec291b802723da6e5fd828bdc1b7d8
junssong <junssong@student.42seoul.kr>
2025-01-19 18:01:39 +0900

    feat: 1991

---
+++ class4/1991.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<pair<int, int> > btree(27, pair<int, int>());
+    for (int i = 0; i < n; i++) {
+        char node, c1, c2;
+        cin >> node >> c1 >> c2;
+        
+    }
+
+
+    return 0;
+}
