2db1004e1522b2be016c739a5465ce708d8670a2
junssong <xms2007@naver.com>
2026-04-07 14:50:44 +0900

    15894 수학은 체육과목 입니다

---
+++ 0x12_Math/15894.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long n;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n;
+	long long ret = n * 3 + n;
+	cout << ret << '\n';
+	return 0;
+}
